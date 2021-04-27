a22f2f352fd2c431a0e5970bbe1526ccbfa7bbd8
zx <zxpstudy@163.com>
2021-04-27 13:28:10 +0800

    perf(hooks): use drop hovering

--- @react-start/hooks/src/useDrop.ts
+++ @react-start/hooks/src/useDrop.ts
@@ -1,4 +1,5 @@
 import { DragEventHandler, useCallback, useMemo, useRef, useState } from "react";
+import { debounce } from "lodash";
 
 const DataKey = "custom-data";
 
@@ -20,10 +21,12 @@ export interface DropAreaOptions<TData> {
   onDragLeave?: DragEventHandler;
 }
 
-export const useDrop = <TData>(options: DropAreaOptions<TData> = {}): [DropProps, DropAreaState] => {
+export const useDrop = <TData>(
+  options: DropAreaOptions<TData> = {},
+  debounceTime?: number,
+): [DropProps, DropAreaState] => {
   const optionsRef = useRef(options);
   optionsRef.current = options;
-  const [isHovering, setIsHovering] = useState<boolean>(false);
 
   const callback = useCallback((dataTransfer: DataTransfer, event: DragEvent) => {
     const dom = dataTransfer.getData(DataKey);
@@ -38,24 +41,36 @@ export const useDrop = <TData>(options: DropAreaOptions<TData> = {}): [DropProps
     }
   }, []);
 
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+  const debounceSetHovering = useCallback(
+    debounce(() => {
+      setIsHovering(false);
+    }, debounceTime || 200),
+    [],
+  );
+
   const dropProps: DropProps = useMemo(() => {
     return {
       onDragOver: (e) => {
         e.preventDefault();
+        e.stopPropagation();
         optionsRef.current.onDragOver && optionsRef.current.onDragOver(e);
+        debounceSetHovering();
       },
       onDragEnter: (e) => {
         e.preventDefault();
+        e.stopPropagation();
         setIsHovering(true);
         optionsRef.current.onDragEnter && optionsRef.current.onDragEnter(e);
       },
       onDragLeave: (e) => {
         e.preventDefault();
-        setIsHovering(false);
+        e.stopPropagation();
         optionsRef.current.onDragLeave && optionsRef.current.onDragLeave(e);
       },
       onDrop: (e) => {
         e.preventDefault();
+        e.stopPropagation();
         e.persist();
         setIsHovering(false);
         callback(e.dataTransfer, e as any);
