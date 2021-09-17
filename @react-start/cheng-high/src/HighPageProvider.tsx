404d029ec68059c52d668ee9928e92a7dc7c3fc3
zx <zxpstudy@163.com>
2021-09-17 15:08:50 +0800

    fix(cheng-high): iconMap && sendEvent

--- @react-start/cheng-high/src/HighPageProvider.tsx
+++ @react-start/cheng-high/src/HighPageProvider.tsx
@@ -1,12 +1,23 @@
-import React, { createContext, ReactNode, useContext, useReducer, Reducer } from "react";
+import React, { createContext, ReactNode, useContext, useReducer, Reducer, useCallback } from "react";
+import { pick, map, reduce, get } from "lodash";
+import { Subject } from "rxjs";
+import { HighEvent } from "./types";
 
 type Values = { [key: string]: any };
 
-type Action = { type: string; payload: any };
+type Action = { type: string; payload?: any };
 
 interface HighPageContextProps {
+  //状态值
   state: Values;
+  //修改状态
   dispatch: (action: Action) => void;
+  //事件处理对象（rx）
+  subject$: Subject<Action>;
+  //发送事件
+  sendEvent: (action: Action) => void;
+  //根据key list 从state中获取对应的数据
+  getStateValues: (items?: HighEvent["receiveStateList"]) => Values | undefined;
 }
 
 const HighPageContext = createContext<HighPageContextProps>({} as any);
@@ -19,5 +30,38 @@ export const HighPageProvider = ({ children }: { children: ReactNode }) => {
     return { ...prevState, [action.type]: action.payload };
   }, {});
 
-  return <HighPageContext.Provider value={{ state, dispatch }}>{children}</HighPageContext.Provider>;
+  const getStateValues = useCallback(
+    (items?: HighEvent["receiveStateList"]) => {
+      if (!items) {
+        return undefined;
+      }
+      const targetValue = pick(
+        state,
+        map(items, (item) => item.name),
+      );
+      return reduce(
+        items,
+        (pair, item) => ({
+          ...item,
+          [item.mapName || item.name]: get(targetValue, item.name),
+        }),
+        {},
+      );
+    },
+    [state],
+  );
+
+  /************************** 事件处理 *****************************/
+
+  const subject$ = new Subject<Action>();
+
+  const sendEvent = useCallback((action: Action) => {
+    subject$.next(action);
+  }, []);
+
+  return (
+    <HighPageContext.Provider value={{ state, dispatch, getStateValues, subject$, sendEvent }}>
+      {children}
+    </HighPageContext.Provider>
+  );
 };
--- @react-start/cheng-high/src/HighProvider.tsx
+++ @react-start/cheng-high/src/HighProvider.tsx
@@ -1,13 +1,28 @@
-import React, { createContext, ForwardRefRenderFunction, FunctionComponent, ReactNode, useContext } from "react";
+import React, {
+  createContext,
+  ForwardRefRenderFunction,
+  FunctionComponent,
+  ReactNode,
+  useCallback,
+  useContext,
+} from "react";
+import { get } from "lodash";
 
 interface HighContextProps {
+  // elements map
   elementsMap?: { [key: string]: FunctionComponent | ForwardRefRenderFunction<any, any> };
+  getElement: (elementName: string) => ReactNode | undefined;
+  // icons map
+  iconMap?: { [key: string]: ReactNode };
+  getIcon: (iconName: string) => ReactNode | undefined;
 }
 
 const HighContext = createContext<HighContextProps>({} as any);
 
 export const useHigh = () => useContext(HighContext);
 
-export const HighProvider = ({ children, elementsMap }: HighContextProps & { children: ReactNode }) => {
-  return <HighContext.Provider value={{ elementsMap }}>{children}</HighContext.Provider>;
+export const HighProvider = ({ children, elementsMap, iconMap }: HighContextProps & { children: ReactNode }) => {
+  const getElement = useCallback((elementName: string) => get(elementsMap, elementName), [elementsMap]);
+  const getIcon = useCallback((iconName: string) => get(iconMap, iconName), [iconMap]);
+  return <HighContext.Provider value={{ elementsMap, getElement, iconMap, getIcon }}>{children}</HighContext.Provider>;
 };
