ef5c40c9241a6d76a20b598bb1eb9880ec0ca2d8
zx <zxpstudy@163.com>
2021-09-23 13:39:06 +0800

    build: cheng-high && cheng-plugin-antd

---
+++ @react-start/cheng-high/rollup.config.js
@@ -0,0 +1,37 @@
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+      },
+      {
+        file: "dist/index.es.js",
+        format: "es",
+        entryFileNames: "[name].es.js",
+        chunkFileNames: "[name]-[hash].es.js",
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions: [".ts", ".tsx", ".mjs", ".js", ".jsx"],
+      }),
+      babel({
+        configFile: "../../babel.config.js",
+        babelHelpers: "bundled",
+        extensions: [".ts", ".tsx", ".mjs", ".js", ".jsx"],
+      }),
+    ],
+    external: ["lodash", "react", "@reactorx/core", "rxjs", "@emotion/react"],
+  },
+  {
+    input: "../../.tmp/@react-start/cheng-high/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
---
+++ @react-start/cheng-plugin-antd/rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+      },
+      {
+        file: "dist/index.es.js",
+        format: "es",
+        entryFileNames: "[name].es.js",
+        chunkFileNames: "[name]-[hash].es.js",
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions: [".ts", ".tsx", ".mjs", ".js", ".jsx"],
+      }),
+      babel({
+        configFile: "../../babel.config.js",
+        babelHelpers: "bundled",
+        extensions: [".ts", ".tsx", ".mjs", ".js", ".jsx"],
+      }),
+    ],
+    external: [
+      "lodash",
+      "react",
+      "antd",
+      "rxjs",
+      "@emotion/react",
+      "@ant-design/icons",
+      "@ant-design/pro-card",
+      "@ant-design/pro-form",
+      "@ant-design/pro-layout",
+      "@ant-design/pro-table",
+      "@react-start/cheng-high",
+    ],
+  },
+  {
+    input: "../../.tmp/@react-start/cheng-high/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
